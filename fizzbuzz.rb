2c64e26f7b595068cd3ebabc162b93fb9f7fed52
Brandon Collins <brandon@flatironschool.com>
2017-09-26 19:10:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,14 @@
-binding.pry
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+   return "FizzBuzz"
+  end
 
-def fizzbuzz
+  if int % 5 == 0
+   return "Buzz"
+  end
+
+  if int % 3 == 0
+   return "Fizz"
+  end
 
 end
